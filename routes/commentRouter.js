5f86967b2c71e0756e58a06dddf40a2c5ffd215a
JAIMIN KATVA <98204346+jaiminkatva@users.noreply.github.com>
2022-07-14 17:45:19 +0530

    Update commentRouter.js

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -2,16 +2,19 @@ const express = require("express");
 const router = express.Router();
 const commentController = require("../controller/commentController");
 
+// insert post router
 router.post("/addComment", commentController.addComment)
 
+// get all post router
 router.get("/getAllComment", commentController.getAllComment)
 
+// update post router
 router.put("/:id", commentController.updateComment)
 
+// delete post router
 router.delete("/:id", commentController.deleteComment)
 
+// get single post router
 router.get("/:id", commentController.getOneComment)
-
-router.get("/getLikeComment", commentController.getLikeComments)
 
 module.exports = router
