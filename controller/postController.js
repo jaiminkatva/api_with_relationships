e1f9c4fe01539a70b5c4c0b11e077eaddc4bca92
JAIMIN KATVA <98204346+jaiminkatva@users.noreply.github.com>
2022-07-14 17:59:02 +0530

    Update postController.js

--- controller/postController.js
+++ controller/postController.js
@@ -2,6 +2,7 @@ const db = require("../models");
 const Post = db.post;
 const Comment = db.comment;
 
+// insert post controller
 const addPost = async(req, res) => {
     const info = {
         title: req.body.title,
@@ -13,34 +14,34 @@ const addPost = async(req, res) => {
     console.log(post)
 }
 
+// get all post controller
 const getAllPost = async(req, res) => {
     const post = await Post.findAll({})
     res.status(200).send(post);
 }
 
+// update post controller
 const updatePost = async(req, res) => {
     id = req.params.id;
     const post = await Post.update(req.body, { where: { id: id } })
     res.status(200).send(post);
 }
 
+// delete post controller
 const deletePost = async(req, res) => {
     id = req.params.id
     await Post.destroy({ where: { id: id } })
     res.status(200).send(`Post deleted successfully id is ${id}`)
 }
 
+// get single post controller
 const getOnePost = async(req, res) => {
     id = req.params.id
     const post = await Post.findOne({ where: { id: id } })
     res.status(200).send(post)
 }
 
-const getPublise = async(req, res) => {
-    const data = await Post.findAll({ where: { publise: true } })
-    res.status(200).send(data)
-}
-
+// get one post to many comments controller
 const oneTomany = async(req, res) => {
 
     let id = req.params.id;
